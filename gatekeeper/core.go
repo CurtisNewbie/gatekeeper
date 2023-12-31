682c579dde2e2ae2868776bb2a96b7b3d2f6cdcc
yongj.zhuang <yongj.zhuang@outlook.com>
2023-12-31 16:20:41 +0800

    Polishing

--- gatekeeper/core.go
+++ gatekeeper/core.go
@@ -11,10 +11,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-const (
-	healthCheckPath = "/health"
-)
-
 var (
 	errPathNotFound = miso.NewErr("Path not found")
 	gatewayClient   *http.Client
@@ -47,128 +43,13 @@ func Bootstrap(args []string) {
 func prepareServer() {
 	common.LoadBuiltinPropagationKeys()
 
-	miso.SetProp(miso.PropServerPropagateInboundTrace, false)      // disable trace propagation, we are the entry point
-	miso.SetProp(miso.PropConsulRegisterDefaultHealthcheck, false) // disable the default health check endpoint to avoid conflicts
-	miso.SetProp(miso.PropConsulHealthcheckUrl, healthCheckPath)   // for consul health check
-	miso.PerfLogExclPath(healthCheckPath)                          // do not measure perf for healthcheck
+	miso.SetProp(miso.PropServerPropagateInboundTrace, false) // disable trace propagation, we are the entry point
 
 	// bootstrap metrics and prometheus stuff manually
 	miso.ManualBootstrapPrometheus()
 
-	miso.RawAny("/*proxyPath", WrapMetricsHandler(func(c *gin.Context, rail miso.Rail) {
-		rail.Debugf("Request: %v %v, headers: %v", c.Request.Method, c.Request.URL.Path, c.Request.Header)
-
-		// check if it's a healthcheck endpoint (for consul), we don't really return anything, so it's fine to expose it
-		if c.Request.URL.Path == healthCheckPath {
-			c.AbortWithStatus(200)
-			return
-		}
-
-		// parse the request path, extract service name, and the relative url for the backend server
-		sp, err := parseServicePath(c.Request.URL.Path)
-		rail.Debugf("parsed servicePath: %+v, err: %v", sp, err)
-
-		if err != nil {
-			rail.Warnf("Invalid request, %v", err)
-			c.AbortWithStatus(404)
-			return
-		}
-
-		pc := NewProxyContext(rail, c)
-		pc.SetAttr(SERVICE_PATH, sp)
-
-		filters := GetFilters()
-		for i := range filters {
-			fr, err := filters[i](pc)
-			if err != nil || !fr.Next {
-				rail.Debugf("request filtered, err: %v, ok: %v", err, fr)
-				if err != nil {
-					miso.DispatchErrJson(c, rail, err)
-					return
-				}
-
-				return // discontinue, the filter should write the response itself, e.g., returning a 403 status code
-			}
-			pc = fr.ProxyContext // replace the ProxyContext, trace may be set
-		}
-
-		// continue propgating the trace
-		rail = pc.Rail
-
-		// set trace back to Gin for the PerfMiddleware, this feels like a hack, but we have to do this
-		c.Set(miso.X_TRACEID, rail.CtxValStr(miso.X_TRACEID))
-		c.Set(miso.X_SPANID, rail.CtxValStr(miso.X_SPANID))
-
-		// route requests dynamically using service discovery
-		relPath := sp.Path
-		if c.Request.URL.RawQuery != "" {
-			relPath += "?" + c.Request.URL.RawQuery
-		}
-		cli := miso.NewTClient(rail, relPath).
-			UseClient(gatewayClient).
-			EnableServiceDiscovery(sp.ServiceName).
-			EnableTracing()
-
-		// propagate all headers to client
-		for k, arr := range c.Request.Header {
-			for i := range arr {
-				cli.AddHeader(k, arr[i])
-			}
-		}
-
-		var r *miso.TResponse
-		switch c.Request.Method {
-		case http.MethodGet:
-			r = cli.Get()
-		case http.MethodPut:
-			r = cli.Put(c.Request.Body)
-		case http.MethodPost:
-			r = cli.Post(c.Request.Body)
-		case http.MethodDelete:
-			r = cli.Delete()
-		case http.MethodHead:
-			r = cli.Head()
-		case http.MethodOptions:
-			r = cli.Options()
-		default:
-			c.AbortWithStatus(404)
-			return
-		}
-
-		if r.Err != nil {
-			rail.Debugf("post proxy request, request failed, err: %v", r.Err)
-			if errors.Is(r.Err, miso.ErrConsulServiceInstanceNotFound) {
-				c.AbortWithStatus(404)
-				return
-			}
-
-			miso.DispatchErrJson(c, rail, r.Err)
-			return
-		}
-		defer r.Close()
-
-		rail.Debugf("post proxy request, proxied response headers: %v, status: %v", r.RespHeader, r.StatusCode)
-
-		// headers from backend servers
-		respHeader := map[string]string{}
-		for k, v := range r.RespHeader {
-			if len(v) > 0 {
-				respHeader[k] = v[0]
-			}
-		}
-
-		// write data from backend to client
-		if r.Resp.Body == nil {
-			c.Status(r.StatusCode)
-			for k, v := range respHeader {
-				c.Header(k, v)
-			}
-		} else {
-			c.DataFromReader(r.StatusCode, r.Resp.ContentLength, c.GetHeader("Content-Type"), r.Resp.Body, respHeader)
-		}
-
-		rail.Debugf("proxy request handled")
-	}))
+	// healthcheck -> metrics -> proxy
+	miso.RawAny("/*proxyPath", WrapHealthHandler(WrapMetricsHandler(ProxyRequestHandler)))
 }
 
 func parseServicePath(url string) (ServicePath, error) {
@@ -197,10 +78,36 @@ func parseServicePath(url string) (ServicePath, error) {
 	}, nil
 }
 
+func WrapHealthHandler(handler miso.RawTRouteHandler) miso.RawTRouteHandler {
+
+	// disable the default health check endpoint to avoid conflicts
+	miso.SetProp(miso.PropConsulRegisterDefaultHealthcheck, false)
+
+	healthcheckPath := miso.GetPropStr(miso.PropConsulHealthcheckUrl)
+	if miso.IsBlankStr(healthcheckPath) {
+		return handler
+	}
+
+	miso.PerfLogExclPath(healthcheckPath)
+	return func(c *gin.Context, rail miso.Rail) {
+		// check if it's a healthcheck endpoint (for consul), we don't really return anything, so it's fine to expose it
+		if c.Request.URL.Path == healthcheckPath {
+			c.AbortWithStatus(200)
+			return
+		}
+
+		handler(c, rail)
+	}
+}
+
 func WrapMetricsHandler(handler miso.RawTRouteHandler) miso.RawTRouteHandler {
 
 	metricsEndpoint := miso.GetPropStr(miso.PropMetricsRoute)
 	miso.PerfLogExclPath(metricsEndpoint)
+
+	if miso.IsBlankStr(metricsEndpoint) {
+		return handler
+	}
 
 	if !miso.GetPropBool(miso.PropMetricsEnabled) {
 		return func(c *gin.Context, rail miso.Rail) {
@@ -229,4 +136,113 @@ func WrapMetricsHandler(handler miso.RawTRouteHandler) miso.RawTRouteHandler {
 
 		handler(c, rail)
 	}
+}
+
+func ProxyRequestHandler(c *gin.Context, rail miso.Rail) {
+	rail.Debugf("Request: %v %v, headers: %v", c.Request.Method, c.Request.URL.Path, c.Request.Header)
+
+	// parse the request path, extract service name, and the relative url for the backend server
+	sp, err := parseServicePath(c.Request.URL.Path)
+	rail.Debugf("parsed servicePath: %+v, err: %v", sp, err)
+
+	if err != nil {
+		rail.Warnf("Invalid request, %v", err)
+		c.AbortWithStatus(404)
+		return
+	}
+
+	pc := NewProxyContext(rail, c)
+	pc.SetAttr(SERVICE_PATH, sp)
+
+	filters := GetFilters()
+	for i := range filters {
+		fr, err := filters[i](pc)
+		if err != nil || !fr.Next {
+			rail.Debugf("request filtered, err: %v, ok: %v", err, fr)
+			if err != nil {
+				miso.DispatchErrJson(c, rail, err)
+				return
+			}
+
+			return // discontinue, the filter should write the response itself, e.g., returning a 403 status code
+		}
+		pc = fr.ProxyContext // replace the ProxyContext, trace may be set
+	}
+
+	// continue propgating the trace
+	rail = pc.Rail
+
+	// set trace back to Gin for the PerfMiddleware, this feels like a hack, but we have to do this
+	c.Set(miso.X_TRACEID, rail.CtxValStr(miso.X_TRACEID))
+	c.Set(miso.X_SPANID, rail.CtxValStr(miso.X_SPANID))
+
+	// route requests dynamically using service discovery
+	relPath := sp.Path
+	if c.Request.URL.RawQuery != "" {
+		relPath += "?" + c.Request.URL.RawQuery
+	}
+	cli := miso.NewTClient(rail, relPath).
+		UseClient(gatewayClient).
+		EnableServiceDiscovery(sp.ServiceName).
+		EnableTracing()
+
+	// propagate all headers to client
+	for k, arr := range c.Request.Header {
+		for i := range arr {
+			cli.AddHeader(k, arr[i])
+		}
+	}
+
+	var r *miso.TResponse
+	switch c.Request.Method {
+	case http.MethodGet:
+		r = cli.Get()
+	case http.MethodPut:
+		r = cli.Put(c.Request.Body)
+	case http.MethodPost:
+		r = cli.Post(c.Request.Body)
+	case http.MethodDelete:
+		r = cli.Delete()
+	case http.MethodHead:
+		r = cli.Head()
+	case http.MethodOptions:
+		r = cli.Options()
+	default:
+		c.AbortWithStatus(404)
+		return
+	}
+
+	if r.Err != nil {
+		rail.Debugf("post proxy request, request failed, err: %v", r.Err)
+		if errors.Is(r.Err, miso.ErrConsulServiceInstanceNotFound) {
+			c.AbortWithStatus(404)
+			return
+		}
+
+		miso.DispatchErrJson(c, rail, r.Err)
+		return
+	}
+	defer r.Close()
+
+	rail.Debugf("post proxy request, proxied response headers: %v, status: %v", r.RespHeader, r.StatusCode)
+
+	// headers from backend servers
+	respHeader := map[string]string{}
+	for k, v := range r.RespHeader {
+		if len(v) > 0 {
+			respHeader[k] = v[0]
+		}
+	}
+
+	// write data from backend to client
+	if r.Resp.Body == nil {
+		c.Status(r.StatusCode)
+		for k, v := range respHeader {
+			c.Header(k, v)
+		}
+	} else {
+		c.DataFromReader(r.StatusCode, r.Resp.ContentLength, c.GetHeader("Content-Type"), r.Resp.Body, respHeader)
+	}
+
+	rail.Debugf("proxy request handled")
 }
